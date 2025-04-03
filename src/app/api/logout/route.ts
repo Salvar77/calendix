980f545dc119d7783afe69a2fe1b0cf2d612ee29
Łukasz <lukaszkus77@gmail.com>
2025-04-03 14:14:35 +0200

    update

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,11 +1,8 @@
 import { session } from "@/libs/session";
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function GET() {
-  await session().set("grantId", null);
-  await session().set("email", null);
-  await session().destroy();
-  revalidatePath("/");
+  const userSession = await session();
+  await userSession.destroy();
   redirect("/?logged-out=1");
 }
